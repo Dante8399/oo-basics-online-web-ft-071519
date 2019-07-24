874bacb414c84b4fa23a203b951f6ac32dab5788
Dante Spencer <spencerdantel@gmail.com>
2019-07-24 13:53:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -20,13 +20,22 @@ class Book
     @page_count = num
   end
   
+  def page_count
+    @page_count
+  end
   
+  def genre=(genre)
+    @genre = genre
+  end
   
-
+  def genre
+    @genre
+  end
   
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
   
-
-
 end
 
 
